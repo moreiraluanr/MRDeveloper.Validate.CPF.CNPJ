58369b258b9d6da0e2594cab811e8a1f5c362d82
Luan Moreira <moreiraluanr@gmail.com>
2020-10-08 00:10:34 -0300

    Package containing validation of a CPF or CNPJ.

---
+++ TesteCpfValidate/Test/TestCNPJ.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PackageValidateCpf.Entities;
+
+namespace TestValidate.Test
+{
+    [TestClass]
+    public class TestCNPJ
+    {
+        private string CnpjValid;
+
+        private string CnpjInvalid;
+
+        public TestCNPJ()
+        {
+            CnpjValid = "26.933.867/0001-93";
+            CnpjInvalid = "99.999.999/9999-99";
+        }
+
+        [TestMethod]
+        public void RetornaTrueSeCnpjForValido()
+        {
+            Assert.AreEqual(true, new CNPJ().Validate(CnpjValid));
+        }
+
+        [TestMethod]
+        public void RetornaFalseSeCnpjForInvalido()
+        {
+            Assert.AreEqual(false, new CNPJ().Validate(CnpjInvalid));
+        }
+    }
+}
